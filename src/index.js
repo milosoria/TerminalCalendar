71031bf7ac93e53680e4829053417a8bc72b8e05
milosoria <camilo.soria@uc.cl>
2022-03-09 20:05:24 -0300

    feat(input inquirer): added recurrence options and the alternative to end the event on a different date

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 #! /usr/bin/env node
-const {readCredentials, insertEvent} = require("./auth/auth.js");
+const { readCredentials, insertEvent } = require("./auth/auth.js");
 const { parseAnswers, isValidDate } = require("./validation.js");
 
 const inquirer = require("inquirer");
-
 
 // VARIABLES DECLARATION
 // valid years range
@@ -104,6 +103,30 @@ const questions = [
     },
     {
         type: "input",
+        name: "recurrence",
+        message: "Do you want it to make it recurrent (y/n)?",
+        default: "n",
+        validate(string) {
+            return string == "y" || string == "n" ? true : false;
+        },
+    },
+    {
+        type: "list",
+        name: "recurrenceType",
+        message: "What type of recurrence do you want?",
+        default: "Yearly",
+        when: (answers) => answers.recurrence == "y",
+        choices: ["YEARLY", "MONTHLY", "WEEKLY"],
+        validate(string) {
+            return string == "YEARLY" ||
+                string == "MONTHLY" ||
+                string == "WEEKLY"
+                ? true
+                : false;
+        },
+    },
+    {
+        type: "input",
         name: "startHour",
         message: "Enter the start hour of the event",
         default: "12:00",
@@ -120,10 +143,86 @@ const questions = [
             return /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/.test(hour);
         },
     },
+    {
+        type: "input",
+        name: "endsOnAnotherDay",
+        message: "Does the event end in a different day (y/n)?",
+        default: "n",
+        validate(string) {
+            return string == "y" || string == "n" ? true : false;
+        },
+    },
+    {
+        type: "input",
+        name: "endDay",
+        message: "Enter the end day of the event",
+        when: (answers) => answers.endsOnAnotherDay == "y",
+        validate(day) {
+            let isNan = true;
+            try {
+                isNan = parseInt(day);
+            } catch (TypeError) {
+                isNan = false;
+            }
+            const pass = isValidDate(day, MONTH, YEAR, VALIDYEARS) && isNan;
+
+            if (pass) {
+                return true;
+            }
+
+            return "Please enter a valid day";
+        },
+    },
+    {
+        type: "input",
+        name: "endMonth",
+        message: "Enter the end month of the event",
+        when: (answers) => answers.endsOnAnotherDay == "y",
+        validate(month) {
+            let pass = true;
+            let isNan = true;
+            try {
+                isNan = isNaN(parseInt(month));
+            } catch (TypeError) {
+                isNan = false;
+            }
+            if (isNan || month < 1 || month > 12) {
+                pass = false;
+            }
+            if (pass) {
+                MONTH = month;
+                return true;
+            }
+            return "Please enter a valid month";
+        },
+    },
+    {
+        type: "input",
+        name: "endYear",
+        message: "Enter the end year of the event",
+        when: (answers) => answers.endsOnAnotherDay == "y",
+        validate(year) {
+            let pass = true;
+            let isNan = true;
+            try {
+                isNan = isNaN(parseInt(year));
+            } catch (TypeError) {
+                isNan = false;
+            }
+            if (isNan || year < VALIDYEARS[0] || year > VALIDYEARS[1]) {
+                pass = false;
+            }
+            if (pass) {
+                YEAR = year;
+                return true;
+            }
+            return "Please enter a valid year";
+        },
+    },
 ];
 
 inquirer.prompt(questions).then((answers) => {
     console.log("\n 🧞 Creating Event For Google Calendar ...");
     EVENT = parseAnswers(answers);
-    readCredentials(EVENT,insertEvent);
+    readCredentials(EVENT, insertEvent);
 });
--- src/validation.js
+++ src/validation.js
@@ -1,7 +1,8 @@
 /**
- * Determines if a year is a leap year
- * @param {year int} year
- */
+* Determines if a year is a leap year
+* @param {year int} year
+* @return {bool}
+*/
 function isLeap(year) {
     // Return true if year
     // is a multiple pf 4 and
@@ -11,11 +12,12 @@ function isLeap(year) {
 }
 
 /**
- * Validates wether a date is valid
- * @param {d int} day
- * @param {m int} month
- * @param {y int} year
- */
+* Validates wether a date is valid
+* @param {d int} day
+* @param {m int} month
+* @param {y int} year
+* @return {bool}
+*/
 function isValidDate(d, m, y, validYears) {
     // If year, month and day
     // are not in given range
@@ -32,56 +34,59 @@ function isValidDate(d, m, y, validYears) {
     }
 
     // Months of April, June,
-    // Sept and Nov must have
+        // Sept and Nov must have
     // number of days less than
     // or equal to 30.
-    if (m == 4 || m == 6 || m == 9 || m == 11) return d <= 30;
+        if (m == 4 || m == 6 || m == 9 || m == 11) return d <= 30;
 
     return true;
 }
 
 /**
- * Inquires for input in order to create an event object
- * @param {answers Object} answers obtained via inquirer prompt
- */
+* Inquires for input in order to create an event object
+* @param {answers Object} answers obtained via inquirer prompt
+* @return {event Object} Event object to make the api call
+*/
 function parseAnswers(answers) {
     const [shour,sminutes] = answers.startHour.split(":");
     const [ehour,eminutes] = answers.endHour.split(":");
+    const recurrenceType = answers.recurrence=="y"? [`RRULE:FREQ=${answers.recurrenceType}`]:[`RRULE:FREQ=DAILY;COUNT=1`]
+    // month 0 index
     answers.month = parseInt(answers.month) - 1; 
 
     return {
-            summary: answers.name,
-            location: "",
-            description: answers.description,
-            start: {
-                dateTime: new Date(
-                    answers.year,
-                    answers.month,
-                    answers.day,
-                    shour,
-                    sminutes
-                ).toISOString(), //"2015-05-28T09:00:00-07:00",
-                timeZone: "America/Santiago",
-            },
-            end: {
-                dateTime: new Date(
-                    answers.year,
-                    answers.month,
-                    answers.day,
-                    ehour,
-                    eminutes
-                ).toISOString(), //"2015-05-28T09:00:00-07:00",
-                timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
-            },
-            recurrence: ["RRULE:FREQ=DAILY;COUNT=1"],
-            attendees: [],
-            reminders: {
-                useDefault: false,
-                overrides: [
-                    { method: "email", minutes: answers.reminder * 24 * 60 },
-                    { method: "popup", minutes: 60 },
-                ],
-            },
+        summary: answers.name,
+        location: "",
+        description: answers.description,
+        start: {
+            dateTime: new Date(
+                answers.year,
+                answers.month,
+                answers.day,
+                shour,
+                sminutes
+            ).toISOString(), //"2015-05-28T09:00:00-07:00",
+            timeZone: "America/Santiago",
+        },
+        end: {
+            dateTime: new Date(
+                answers.endsOnAnotherDay == "yes" ? answers.endYear : answers.year,
+                answers.endsOnAnotherDay == "yes" ? answers.endMonth : answers.month,
+                answers.endsOnAnotherDay == "yes" ? answers.endDay : answers.day,
+                ehour,
+                eminutes
+            ).toISOString(), //Format Example: "2015-05-28T09:00:00-07:00",
+            timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+        },
+        recurrence: recurrenceType,
+        attendees: [],
+        reminders: {
+            useDefault: false,
+            overrides: [
+                { method: "email", minutes: answers.reminder * 24 * 60 },
+                { method: "popup", minutes: 60 },
+            ],
+        },
     };
 }
 
